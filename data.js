8880e37b05d38f59d7e49429d3a23d22afca4200
JOurspectre <93581999+JOurspectre@users.noreply.github.com>
2025-11-21 17:07:00 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dine-in-b",
+      "name": "Dine In B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.5155331832516197,
+        "pitch": 0.20094520726261678,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7450726139157346,
+          "pitch": 0.26599119763500667,
+          "rotation": 5.497787143782138,
+          "target": "1-kitchen-a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7150930043091197,
+          "pitch": -0.24696818436609647,
+          "title": "Speaker # 6 - Outside",
+          "text": "For Music"
+        },
+        {
+          "yaw": -2.3355302773889033,
+          "pitch": -0.19045907659241124,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": -1.3999693371111395,
+          "pitch": -0.20505797148585003,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": 1.56714250610717,
+          "pitch": -0.26882052307460214,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": 0.36673245574041147,
+          "pitch": -0.2187074684289385,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": -0.7263277136186215,
+          "pitch": -0.10790177062158435,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": -0.08520342901324973,
+          "pitch": -0.11386678661687633,
+          "title": "Speaker",
+          "text": "For Music"
+        },
+        {
+          "yaw": 2.0191376534707075,
+          "pitch": -0.21985927264799976,
+          "title": "Speaker (Outside)",
+          "text": "Speaker Outside<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-kitchen-a",
+      "name": "Kitchen A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.4751341880653186,
+        "pitch": -0.16599634407887365,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4278690743520759,
+          "pitch": 0.4065342089889601,
+          "rotation": 0,
+          "target": "0-dine-in-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3038681482996477,
+          "pitch": -0.5584819147057534,
+          "title": "Cabinet Of Amplifier and CCTV NVR",
+          "text": "All wires will start here"
+        }
+      ]
+    }
+  ],
+  "name": "PITX Speaker Location",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
